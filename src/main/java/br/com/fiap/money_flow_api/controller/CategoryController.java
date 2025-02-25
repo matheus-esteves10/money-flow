05109d48a1e2d7f5cacbf0c32621c1752b7bad8a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-25 20:46:13 -0300

    avanço na controller

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -1,23 +1,41 @@
 package br.com.fiap.money_flow_api.controller;
 
 import br.com.fiap.money_flow_api.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
+    private List<Category> repository = new ArrayList<>();
+
     @GetMapping()
     public List<Category> getCategories() {
-        return List.of(
-                new Category(1L, "Educação", "book"),
-                new Category(2L, "Lazer", "casino"),
-                new Category(3L, "Salário", "payments")
+        return repository;
+    }
 
-        );
+    @PostMapping()
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        System.out.println("Cadastrando... " + category.getName());
+        repository.add(category);
+        return ResponseEntity.status(201).body(category);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getById(@PathVariable Long id) {
+        System.out.println("Buscando " + id);
+        var category = repository.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+
+        if(category.isEmpty()){
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.status(200).body(category.get());
     }
 }
--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -1,5 +1,7 @@
 package br.com.fiap.money_flow_api.model;
 
+import java.util.Random;
+
 public class Category {
 
     private Long id;
@@ -7,7 +9,7 @@ public class Category {
     private String icon;
 
     public Category(Long id, String name, String icon) {
-        this.id = id;
+        this.id = (id == null) ? new Random().nextLong() : id;
         this.name = name;
         this.icon = icon;
     }
